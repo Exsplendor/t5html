5bf18bb8dffe316f175eb1136f8e7080228c03cc
Exsplendor <splendor.floris@proton.me>
2023-01-19 12:16:59 +0100

    add: convenience script for pytest (reruns pytest indefinitly)

---
+++ meta/script/watchtests.bash
@@ -0,0 +1,28 @@
+#!/usr/bin/bash
+HELP="
+Usage:
+    
+    $0 
+"
+SCRIPT_NEEDS=(bash watch pytest)
+
+
+# Scripting Helpers
+_abort()   { echo "$1"; exit 1; }
+_trycmd()  { which $1 >/dev/null 2>&1; }
+
+
+# Abort Script On Missing Arguments Or Required Programs 
+
+    [ "$#" -gt 0 ] && _abort "$HELP"
+    for CMD in ${SCRIPT_NEEDS[@]}; do
+        _trycmd $CMD || PLEASE_INSTALL+="$CMD "
+    done
+
+    test -n "$PLEASE_INSTALL" \
+        && _abort "Failed to check due to missing programs: $PLEASE_INSTALL" 
+
+## restart pytest every 5 seconds
+    watch -n5 pytest
+# vi: et sw=4 ts=4 list
+
